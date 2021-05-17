78e15f00b14e3800c4c6c8636338006484caf2a0
zatan <zungkanmanee@gmail.com>
2021-05-17 12:40:19 +0700

    chromdriver need to be in path

--- scrape.py
+++ scrape.py
@@ -1,75 +1,187 @@
 import os
-import requests # pip install requests #to sent GET requests
-from bs4 import BeautifulSoup # pip install bs4 #to parse html(getting data out from html, xml or other markup languages)
+import time
 
-# user can input a search keyword and the count of images required
-# download images from google search image
-Google_Image = 'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
+import io
+import hashlib
+import signal
+from glob import glob
+import requests
 
-# The User-Agent request header contains a characteristic string 
-# that allows the network protocol peers to identify the application type, 
-# operating system, and software version of the requesting software user agent.
-# needed for google search
-u_agnt = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-    'Accept-Encoding': 'none',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Connection': 'keep-alive',
-} #write: 'my user agent' in browser to get your browser user agent details
+from PIL import Image
+from selenium import webdriver
 
-Image_Folder = 'interlaken' # Creating a folder to save the images and assigning to a variable for further use
+number_of_images = 400
+GET_IMAGE_TIMEOUT = 2
+SLEEP_BETWEEN_INTERACTIONS = 0.1
+SLEEP_BEFORE_MORE = 5
+IMAGE_QUALITY = 85
 
-def main():
-    if not os.path.exists(Image_Folder):
-        os.mkdir(Image_Folder)
-    download_images()
+output_path = "/path/to/your/image/directory"
 
-def download_images():
-    data = input('Enter your search keyword: ')
-    num_images = int(input('Enter the number of images you want: '))
-    
-    print('Searching Images....')
-    
-    search_url = Google_Image + 'q=' + data #'q=' because its a query
-    
-    # request url, without u_agnt the permission gets denied
-    response = requests.get(search_url, headers=u_agnt)
-    html = response.text #To get actual result i.e. to read the html data in text mode
-    
-    # find all img where class='rg_i Q4LuWd'
-    b_soup = BeautifulSoup(html, 'html.parser') #html.parser is used to parse/extract features from HTML files
-    results = b_soup.findAll('img', {'class': 'rg_i Q4LuWd'})
-    
-    #extract the links of requested number of images with 'data-src' attribute and appended those links to a list 'imagelinks'
-    #allow to continue the loop in case query fails for non-data-src attributes
-    count = 0
-    imagelinks= []
-    for res in results:
-        try:
-            link = res['data-src']
-            imagelinks.append(link)
-            count = count + 1
-            if (count >= num_images):
+search_terms = [
+    "black and white magic symbol icon",
+    "black and white arcane symbol icon",
+    "black and white mystical symbol",
+    "black and white useful magic symbols icon",
+    "black and white ancient magic sybol icon",
+    "black and white key of solomn symbol icon",
+    "black and white historic magic symbol icon",
+    "black and white symbols of demons icon",
+    "black and white magic symbols from book of enoch",]
+
+dirs = glob(output_path + "*")
+dirs = [dir.split("/")[-1].replace("_", " ") for dir in dirs]
+search_terms = [term for term in search_terms if term not in dirs]
+
+wd = webdriver.Chrome()
+wd.get("https://google.com")
+
+class timeout:
+    def __init__(self, seconds=1, error_message="Timeout"):
+        self.seconds = seconds
+        self.error_message = error_message
+
+    def handle_timeout(self, signum, frame):
+        raise TimeoutError(self.error_message)
+
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+
+def fetch_image_urls(
+    query: str,
+    max_links_to_fetch: int,
+    wd: webdriver,
+    sleep_between_interactions: int = 1,
+):
+    def scroll_to_end(wd):
+        wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(sleep_between_interactions)
+
+    # Build the Google Query.
+    search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
+
+    # load the page
+    wd.get(search_url.format(q=query))
+
+    # Declared as a set, to prevent duplicates.
+    image_urls = set()
+    image_count = 0
+    results_start = 0
+    while image_count < max_links_to_fetch:
+        scroll_to_end(wd)
+
+        # Get all image thumbnail results
+        thumbnail_results = wd.find_elements_by_css_selector("img.Q4LuWd")
+        number_results = len(thumbnail_results)
+
+        print(
+            f"Found: {number_results} search results. Extracting links from {results_start}:{number_results}"
+        )
+
+        # Loop through image thumbnail identified
+        for img in thumbnail_results[results_start:number_results]:
+            # Try to click every thumbnail such that we can get the real image behind it.
+            try:
+                img.click()
+                time.sleep(sleep_between_interactions)
+            except Exception:
+                continue
+
+            # Extract image urls
+            actual_images = wd.find_elements_by_css_selector("img.n3VNCb")
+            for actual_image in actual_images:
+                if actual_image.get_attribute(
+                    "src"
+                ) and "http" in actual_image.get_attribute("src"):
+                    image_urls.add(actual_image.get_attribute("src"))
+
+            image_count = len(image_urls)
+
+            # If the number images found exceeds our `num_of_images`, end the seaerch.
+            if len(image_urls) >= max_links_to_fetch:
+                print(f"Found: {len(image_urls)} image links, done!")
                 break
-            
-        except KeyError:
-            continue
-    
-    print(f'Found {len(imagelinks)} images')
-    print('Start downloading...')
+        else:
+            # If we haven't found all the images we want, let's look for more.
+            print("Found:", len(image_urls), "image links, looking for more ...")
+            time.sleep(SLEEP_BEFORE_MORE)
 
-    for i, imagelink in enumerate(imagelinks):
-        # open each image link and save the file
-        response = requests.get(imagelink)
-        
-        imagename = Image_Folder + '/' + data + str(i+1) + '.jpg'
-        with open(imagename, 'wb') as file:
-            file.write(response.content)
+            # Check for button signifying no more images.
+            not_what_you_want_button = ""
+            try:
+                not_what_you_want_button = wd.find_element_by_css_selector(".r0zKGf")
+            except:
+                pass
 
-    print('Download Completed!')
-    
+            # If there are no more images return.
+            if not_what_you_want_button:
+                print("No more images available.")
+                return image_urls
 
-if __name__ == '__main__':
-    main()
+            # If there is a "Load More" button, click it.
+            load_more_button = wd.find_element_by_css_selector(".mye4qd")
+            if load_more_button and not not_what_you_want_button:
+                wd.execute_script("document.querySelector('.mye4qd').click();")
+
+        # Move the result startpoint further down.
+        results_start = len(thumbnail_results)
+
+    return image_urls
+
+
+def persist_image(folder_path: str, url: str):
+    try:
+        print("Getting image")
+        # Download the image.  If timeout is exceeded, throw an error.
+        with timeout(GET_IMAGE_TIMEOUT):
+            image_content = requests.get(url).content
+
+    except Exception as e:
+        print(f"ERROR - Could not download {url} - {e}")
+
+    try:
+        # Convert the image into a bit stream, then save it.
+        image_file = io.BytesIO(image_content)
+        image = Image.open(image_file).convert("RGB")
+        # Create a unique filepath from the contents of the image.
+        file_path = os.path.join(
+            folder_path, hashlib.sha1(image_content).hexdigest()[:10] + ".jpg"
+        )
+        with open(file_path, "wb") as f:
+            image.save(f, "JPEG", quality=IMAGE_QUALITY)
+        print(f"SUCCESS - saved {url} - as {file_path}")
+    except Exception as e:
+        print(f"ERROR - Could not save {url} - {e}")
+
+def search_and_download(search_term: str, target_path="./images", number_images=5):
+    # Create a folder name.
+    target_folder = os.path.join(target_path, "_".join(search_term.lower().split(" ")))
+
+    # Create image folder if needed.
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+
+    # Open Chrome
+    with webdriver.Chrome() as wd:
+        # Search for images URLs.
+        res = fetch_image_urls(
+            search_term,
+            number_images,
+            wd=wd,
+            sleep_between_interactions=SLEEP_BETWEEN_INTERACTIONS,
+        )
+
+        # Download the images.
+        if res is not None:
+            for elem in res:
+                persist_image(target_folder, elem)
+        else:
+            print(f"Failed to return links for term: {search_term}")
+
+# Loop through all the search terms.
+for term in search_terms:
+    search_and_download(term, output_path, number_of_images)
