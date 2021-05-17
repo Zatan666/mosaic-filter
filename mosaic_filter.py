497fe18c386934708d50a378f55ea6ed0a4664bc
zatan <zungkanmanee@gmail.com>
2021-05-17 09:12:15 +0700

    paste each grid

--- mosaic_filter.py
+++ mosaic_filter.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
-from sizing import dimention
+
 from PIL import Image
+from indices import pixels_by_size
+from mean_rgb_values import mean_img_rgb
 
 im = Image.open('./pic/test.png')
-print(dimention(im))
+indices = pixels_by_size(im, (64, 64))
+print('number of grid by size 64x64', len(indices))
+
+for index in indices:
+    
+    grid_rgb =  mean_img_rgb(im.crop(index))
+    piece = Image.new('RGB', (63, 63), grid_rgb)
+    im.paste(piece, index)
+    
+im.show()
 im.close()
+
+
+
