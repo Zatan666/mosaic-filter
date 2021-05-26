9a764a206687c4cd3b92c6fb0526ab9d58c248a4
newbie-k <aomkhung0@hotmail.com>
2021-05-26 13:15:17 +0700

    fix calculation error by correcting size of grid

--- compare.py
+++ compare.py
@@ -5,7 +5,7 @@ from PIL import Image
 import os
 import numpy
 
-def compare1(grid_rgb: ('r','g','b'), list_rgb:[('r','g','b')]):
+def compare1(grid_rgb: (int, int, int), list_rgb:[(int, int, int)]):
     diffs = [sum(abs(i - j) for i, j in zip(grid_rgb, rgb))
                             for rgb in list_rgb]
     min_diff = min(diffs)
@@ -31,12 +31,12 @@ def list_mean_rgb(q: 'search' = None):
     return files, list_mean
 
 def screening(grid_rgb,pieces_rgb):
-    rgb_range = 70
+    rgb_range = 30
     r_screen = []
     g_screen = []
     b_screen = []
 
-    for piece_rgb in pieces_rgb[1]:
+    for piece_rgb in pieces_rgb:
         if piece_rgb[0] in range(grid_rgb[0]- rgb_range,grid_rgb[0]+rgb_range+1):
             r_screen.append(piece_rgb)
 
@@ -54,41 +54,48 @@ def screening(grid_rgb,pieces_rgb):
             diff.append(tuple(numpy.subtract(grid_rgb , piece_rgb)))
         min_diff = min(diff)
         min_index = diff.index(min_diff)
-        min_pieces = b_screen[min_index]
+        a = b_screen[min_index]
+        min_index = pieces_rgb.index(a)
+        min_piece = pieces_rgb[min_index]
         print('con 1')
-        return min_pieces, min_index
-    elif len(b_screen) == 0 :
+        print(min_piece)
+        return min_piece, min_index
+    elif len(b_screen) == 0 and len(g_screen) >= 1:
         diff = []
         for piece_rgb in g_screen:
             diff.append(tuple(numpy.subtract(grid_rgb , piece_rgb)))
         min_diff = min(diff)
         min_index = diff.index(min_diff)
-        min_pieces = g_screen[min_index]
+        a = g_screen[min_index]
+        min_index = pieces_rgb.index(a)
+        min_piece = pieces_rgb[min_index]
         print('con 2')
-        return min_pieces, min_index
+        print(min_piece)
+        return min_piece, min_index
     else:
+        min_piece, min_index = compare1(grid_rgb,pieces_rgb)
         print('con 3')
-        return b_screen,pieces_rgb[1].index(b_screen[0])
+        print(min_piece)
+        return min_piece, min_index
+
 
 from os import listdir
-images = listdir('./pic/')
-
-l = list_mean_rgb()
+images, l = list_mean_rgb()
 
 if __name__ == '__main__':
     im = Image.open('./pic/test.png')
+    print(im.size, '\n')
     indices = pixels_by_size(im, (64, 64))
     print('number of grid by size 64x64', len(indices))
     for index in indices:
+        print(index)
         grid = im.crop(index)
         grid_rgb = mean_img_rgb(grid)
         piece1 = images[screening(grid_rgb,l)[1]]
         # piece = Image.new('RGB', (64, 64), grid_rgb)
         piece = Image.open('./pic/'+piece1)
         im.paste(piece, index[:2])
-
+        print(grid_rgb)
+        print()
     im.show()
     im.close()
-
-
-
