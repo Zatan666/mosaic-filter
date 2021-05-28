e6710b32444bb678d2f484cc289697d422d10cbb
newbie-k <aomkhung0@hotmail.com>
2021-05-28 12:05:22 +0700

    upload scraped picture

--- scrape_firefox.py
+++ scrape_firefox.py
@@ -1,32 +1,51 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 import requests
 import os.path
 
 
-q = 'doom'
+q = 'doom+game'
 url = f'https://www.google.com/search?&tbm=isch&q={q}'
-num = 10
+num = 1000
 
-driver = webdriver.Firefox()
+#opt = webdriver.firefox.options.Options()
+#opt.headless = True
+opt = None
+driver = webdriver.Firefox(options=opt)
 driver.get(url)
 
 # 3.1: initialize folders
 if not os.path.isdir('pic'):
     os.mkdir('pic')
-if not os.path.isdir(q):
+if not os.path.isdir(f'pic/{q}'):
     os.mkdir(f'pic/{q}')
 
 # 1st: load page untill number of picture reachs requirement
 
+driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
 elems = driver.find_elements_by_css_selector("img.Q4LuWd")
 num_count = len(elems)
+curr_count = 0
 while num_count < num:
     driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
     driver.implicitly_wait(5)
     elems = driver.find_elements_by_css_selector("img.Q4LuWd")
     num_count = len(elems)
+    print(num_count)
+    if curr_count == num_count:
+        driver.find_element_by_css_selector("input.mye4qd").click()
+        try:
+            driver.find_element_by_css_selector("div.DwpMZe[data-status='3']")
+            break
+        except:
+            pass
+    curr_count = num_count
+    #print(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.YstHxe")))
+        #driver.find_element_by_css_selector("input.mye4qd").click()
+    time.sleep(2)
 selected = driver.find_elements_by_css_selector("img.Q4LuWd")[:num]
 
 
@@ -35,7 +54,7 @@ def image_loaded(driver):
     number = driver.execute_script("return document.images.length")
     return all(driver.execute_script(f"return document.images[{i}].complete") for i in range(number))
 
-
+# 3nd: go to each element and download it
 for i, img in enumerate(selected, 1):
     try:
         img.click()
@@ -46,7 +65,7 @@ for i, img in enumerate(selected, 1):
         pass
     actual_image = driver.find_element_by_xpath("//div[@class='tvh9oe BIB1wf']//img[@class='n3VNCb']")
     image = actual_image.get_attribute('src')
-    #print(image)
+    print(image[:20])
 
     r = requests.get(image)
 
