c66e99a8bcb8745eec794f6d8d96074373e7ea04
newbie-k <aomkhung0@hotmail.com>
2021-05-28 09:25:34 +0700

    result testing

--- mosaic_filter.py
+++ mosaic_filter.py
@@ -4,7 +4,7 @@ from PIL import Image
 from indices import pixels_by_size
 from mean_rgb_values import mean_img_rgb
 from coloring import boost_color
-from compare import list_mean_rgb,compare1,screening
+from compare import list_mean_rgb, compare1, screening
 
 
 if __name__ == '__main__':
@@ -21,8 +21,10 @@ if __name__ == '__main__':
         piece1 = images[compare1(grid_rgb, l)[1]]
         # piece = Image.new('RGB', (64, 64), grid_rgb)
         piece = Image.open('./pic/'+piece1)
-        #color = boost_color(piece, grid_rgb)
-        im.paste(piece, index[:2])
+        piece_resize = piece.resize((64, 64))
+        piece_boost = boost_color(piece_resize, grid_rgb)
+        #im.paste(piece_resize, index[:2])
+        im.paste(piece_boost, index[:2])
         # print(grid_rgb)
         # print()
     im.show()
